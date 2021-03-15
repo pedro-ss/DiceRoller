c06e1d7703869289b97e48caf110dec5b97ff960
Pedro-ss <pedross.tool@gmail.com>
2021-03-15 15:07:57 -0300

    Projeto DiceRoller

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val girarButton: Button = findViewById(R.id.grirar_button)
+        //Toast.makeText(this, "Botão clicado", Toast.LENGTH_SHORT).show()
+
+        girarButton.setOnClickListener {
+            girarDado()
+        }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun girarDado() {
+        //val resultText: TextView = findViewById(R.id.result_text)
+        //resultText.text = randomInt.toString()
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
